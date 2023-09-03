19a134e948c4365650b74101cd36e2ce06bcb9ca
sihyeokpark <exon.indiv@gmail.com>
2023-09-03 21:38:14 +0900

    🔥

--- src/pages/Member.tsx
+++ src/pages/Member.tsx
@@ -13,11 +13,6 @@ export function Member() {
         </motion.div>
         <motion.div className='member' whileHover={{ scale: 1.05 }}>
           <img src={logo} alt='avatar'/>
-          <p><strong>BEEP</strong></p>
-          <p>Rev</p>
-        </motion.div>
-        <motion.div className='member' whileHover={{ scale: 1.05 }}>
-          <img src={logo} alt='avatar'/>
           <p><strong>exon</strong></p>
           <p>Web</p>
         </motion.div>
@@ -28,8 +23,13 @@ export function Member() {
         </motion.div>
         <motion.div className='member' whileHover={{ scale: 1.05 }}>
           <img src={logo} alt='avatar'/>
-          <p><strong>나는재영</strong></p>
+          <p><strong>s0lwoo</strong></p>
           <p>Crypto</p>
+        </motion.div>
+        <motion.div className='member' whileHover={{ scale: 1.05 }}>
+          <img src={logo} alt='avatar'/>
+          <p><strong>BEEP</strong></p>
+          <p>Rev</p>
         </motion.div>
       </div>
     </>
